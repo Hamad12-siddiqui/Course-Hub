2a44d45dc037e4af76b3c6380b8e028bb1f58266
Muhsan Sattar <muhsansattar97@gmail.com>
2025-09-03 16:00:47 +0500

    fix: make new component in aboutus section

--- app/modules/About_us/About_us.js
+++ app/modules/About_us/About_us.js
@@ -1,12 +1,14 @@
 import Navbar from '@/app/components/Navbar'
 import React from 'react'
 import Herosection from './Herosection'
+import OnlineLearning from './Online-Learning'
 
 const About_us = () => {
   return (
     <>
     <Navbar />
     <Herosection/>
+    <OnlineLearning />
     
     </>
   )
